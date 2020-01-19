ea6902d3a8ddb83db7b8ac730453c99e2309f693
Rus Hughes <russell.hughes@gmail.com>
2020-01-19 13:12:42 +0100

    require scss in main

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store } from './store.js'
+import './assets/scss/app.scss'
+
 const fb = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false
