115290d5b14c67d39246bdec2beba77df6d1a01b
Rus Hughes <russell.hughes@gmail.com>
2020-01-19 13:31:57 +0100

    create logout

--- src/store.js
+++ src/store.js
@@ -1,16 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const fb = require('./firebaseConfig.js')
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-
+    currentUser: null,
+    userProfile: {}
   },
   actions: {
-
+    clearData ({ commit }) {
+      commit('setCurrentUser', null)
+      commit('setUserProfile', {})
+    },
+    fetchUserProfile ({ commit, state }) {
+      fb.usersCollection.doc(state.currentUser.uid).get().then(res => {
+        commit('setUserProfile', res.data())
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   },
   mutations: {
-
+    setCurrentUser (state, val) {
+      state.currentUser = val
+    },
+    setUserProfile (state, val) {
+      state.userProfile = val
+    }
   }
 })
