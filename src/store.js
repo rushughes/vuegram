b445ac5b59e938d409ccde149b5b49d4bf79b9bb
Rus Hughes <russell.hughes@gmail.com>
2020-01-20 13:24:44 +0100

    we can now change settings!

--- src/store.js
+++ src/store.js
@@ -67,6 +67,31 @@ export const store = new Vuex.Store({
       }).catch(err => {
         console.log(err)
       })
+    },
+    updateProfile ({ commit, state }, data) {
+      let name = data.name
+      let title = data.title
+
+      fb.usersCollection.doc(state.currentUser.uid).update({ name, title }).then(user => {
+        // update all posts by user to reflect new name
+        fb.postsCollection.where('userId', '==', state.currentUser.uid).get().then(docs => {
+          docs.forEach(doc => {
+            fb.postsCollection.doc(doc.id).update({
+              userName: name
+            })
+          })
+        })
+        // update all comments by user to reflect new name
+        fb.commentsCollection.where('userId', '==', state.currentUser.uid).get().then(docs => {
+          docs.forEach(doc => {
+            fb.commentsCollection.doc(doc.id).update({
+              userName: name
+            })
+          })
+        })
+      }).catch(err => {
+        console.log(err)
+      })
     }
   },
   mutations: {
